af1257107322f0f8813e20b1d59a5e9215d9c3a2
c-russell-II <95008323+c-russell-II@users.noreply.github.com>
2022-10-26 12:56:58 -0700

    Add Walk/Roll Anims

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,98 @@
-import Phaser from 'phaser';
-
+import Phaser from "phaser";
 export default class Demo extends Phaser.Scene {
-  constructor() {
-    super('GameScene');
-  }
-
-  preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
-  }
-
-  create() {
-    const logo = this.add.image(400, 70, 'logo');
-
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
-      repeat: -1
-    });
-  }
+	constructor() {
+		super("GameScene");
+	}
+	player: any;
+	preload() {
+		this.load.atlas(
+			"knight",
+			"assets/splitKnight.png",
+			"assets/splitKnight.json"
+		);
+	}
+	create() {
+		this.matter.world.setBounds(0, 0, 800, 600);
+		const floor = this.add.rectangle(400, 500, 800, 100, 0x666666);
+		this.matter.add.gameObject(floor, { isStatic: true });
+		const idleFrames = this.anims.generateFrameNames("knight", {
+			start: 0,
+			end: 9,
+			zeroPad: 1,
+			prefix: "_Idle/_Idle-",
+			suffix: ".png",
+		});
+		this.anims.create({
+			key: "idle",
+			frames: idleFrames,
+			frameRate: 10,
+			repeat: -1,
+		});
+		const runFrames = this.anims.generateFrameNames("knight", {
+			start: 0,
+			end: 9,
+			zeroPad: 1,
+			prefix: "_Run/_Run-",
+			suffix: ".png",
+		});
+		this.anims.create({
+			key: "run",
+			frames: runFrames,
+			frameRate: 10,
+			repeat: -1,
+		});
+		const rollFrames = this.anims.generateFrameNames("knight", {
+			start: 0,
+			end: 11,
+			prefix: "_Roll/_Roll-",
+			suffix: ".png",
+		});
+		this.anims.create({
+			key: "roll",
+			frames: rollFrames,
+			frameRate: 20,
+			repeat: 0,
+		});
+		this.player = this.matter.add.sprite(
+			100,
+			450,
+			"knight",
+			"_Idle/_Idle-0.png"
+		);
+		this.player.play("idle");
+		this.player.setInteractive();
+		this.player.cursors = this.input.keyboard.createCursorKeys();
+		this.input.keyboard.on("keydown-SHIFT", () => {
+			console.log("SHIFT pressed");
+			if (this.player.anims.currentAnim.key == "roll") {
+				return;
+			}
+			if (this.player.anims.currentAnim.key == "run") {
+				this.player.play("roll", false);
+			}
+		});
+	}
+	update() {
+		if (this.player.anims.currentAnim.key === "roll") {
+			const direction = this.player.flipX ? -1 : 1;
+			this.player.x += 3 * direction;
+			this.player.on(
+				"animationcomplete",
+				(animation: Phaser.Animations.Animation) => {
+					this.player.play("idle");
+				}
+			);
+		} else {
+			// Moves left and right with arrow keys
+			if (this.player.cursors.left.isDown) {
+				this.player.x -= 2;
+				this.player.play("run", true).flipX = true;
+			} else if (this.player.cursors.right.isDown) {
+				this.player.x += 2;
+				this.player.play("run", true).flipX = false;
+			} else {
+				this.player.play("idle", true);
+			}
+		}
+	}
 }
